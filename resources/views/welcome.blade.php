12b909275cd40551d350bfd147dd19f01ecb603d
2639631600 <2639631600@qq.com>
2018-08-23 12:49:25 +0000

    修改首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 估价报告管理系统
                 </div>
 
                 <div class="links">
